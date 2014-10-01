19ce5f40c4cac6ba1741e2ab2f29ab71b9993109
javier <javrod83@gmail.com>
2014-10-01 01:00:09 -0300

    testing rgb_common annode

---
+++ rgb_anode_handling/rgb.ino
@@ -0,0 +1,12 @@
+
+
+void rgb_setup()
+  {
+      pinMode(GREEN, OUTPUT);
+      pinMode(BLUE, OUTPUT);
+      pinMode(RED, OUTPUT);
+      digitalWrite(GREEN, HIGH);
+      digitalWrite(BLUE, HIGH);
+      digitalWrite(RED, HIGH);
+  }
+
---
+++ rgb_anode_handling/rgb_anode_handling.ino
@@ -0,0 +1,52 @@
+
+#define GREEN 6
+#define BLUE 5
+#define RED 3
+#define delayTime 200
+void setup() {
+
+rgb_setup();
+}
+
+int redVal;
+int blueVal;
+int greenVal;
+ 
+void loop() {
+ 
+  int redVal = 255;
+  int blueVal = 0;
+  int greenVal = 0;
+  for( int i = 0 ; i < 255 ; i += 1 ){
+    greenVal += 1;
+    redVal -= 1;
+    analogWrite( GREEN, 255 - greenVal );
+    analogWrite( RED, 255 - redVal );
+
+    delay( delayTime );
+  }
+ 
+  redVal = 0;
+  blueVal = 0;
+  greenVal = 255;
+  for( int i = 0 ; i < 255 ; i += 1 ){
+    blueVal += 1;
+    greenVal -= 1;
+    analogWrite( BLUE, 255 - blueVal );
+    analogWrite( GREEN, 255 - greenVal );
+
+    delay( delayTime );
+  }
+ 
+  redVal = 0;
+  blueVal = 255;
+  greenVal = 0;
+  for( int i = 0 ; i < 255 ; i += 1 ){
+    redVal += 1;
+    blueVal -= 1;
+    analogWrite( RED, 255 - redVal );
+    analogWrite( BLUE, 255 - blueVal );
+
+    delay( delayTime );
+  }
+}
